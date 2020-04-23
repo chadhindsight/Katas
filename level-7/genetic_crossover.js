eee2f478cfe51a6b55904735870f67bd7572ac11
Chad Hinds <chadrickj8@gmail.com>
2020-04-23 19:45:12 -0400

    Complete Kata

--- level-7/genetic_crossover.js
+++ level-7/genetic_crossover.js
@@ -1,1 +1,15 @@
-// swap one's cromosome part with another in a specific given index to the end
+//  to implement a function crossover that receives two chromosomes chromosome1, chromosome2 and a zero-based index and it has to return an array with 
+// the crossover result on both chromosomes [chromosome1, chromosome2].
+
+const crossover = (chromosome1, chromosome2, index) => {
+    let x = chromosome1.split('')
+    let y = chromosome2.split('')
+
+    let swapx = x.splice(index, chromosome1.length)
+    let swapy = y.splice(index, chromosome2.length)
+
+    let newX = x.concat(swapy).join('')
+    let newY = y.concat(swapx).join('')
+
+    return [newX, newY]
+}
