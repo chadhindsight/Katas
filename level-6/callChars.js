a4a229fd7e76162bf26ee35e6a1f48f5444155cf
Chad Hinds <chadrickj8@gmail.com>
2020-04-24 07:48:36 -0400

    check if an individual character has match

--- level-6/callChars.js
+++ level-6/callChars.js
@@ -2,6 +2,7 @@
 function regexContainsAll(str) {
     let chars = str.split('');
     let pattern = '';
+    // See if an individual character has whatever that string on 7 is
     chars.forEach(c => {
         pattern += `(?=.*${c})`;
     });
