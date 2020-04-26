c905d8fad145e13fbf6f08f6a2a956f16c3f87d5
Chad Hinds <chadrickj8@gmail.com>
2020-04-26 10:28:11 -0400

    Love Language kata

---
+++ level-6/love.js
@@ -0,0 +1,9 @@
+// Love Language
+const loveLanguage = (partner, weeks) => {
+    const loves = ["words", "acts", "gifts", "time", "touch"].map(lang => ({ lang, count: 0 }));
+    for (let day = 0; day < weeks * 7; day++) {
+        const love = loves[day % 5];
+        love.count += partner.response(love.lang) === 'positive' ? 1 : 0;
+    }
+    return loves.reduce((a, b) => a.count > b.count ? a : b).lang;
+}
