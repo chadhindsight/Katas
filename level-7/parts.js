a5d04755ae0f77c2761ea71bf1137cac20935f4a
Chad Hinds <chadrickj8@gmail.com>
2020-05-07 08:38:14 -0400

    New level 7 Kata

---
+++ level-7/parts.js
@@ -0,0 +1,9 @@
+// The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
+var splitInParts = function (s, partLength) {
+    var parts = [];
+    var array = s.split('');
+    while (array.length) {
+        parts.push(array.splice(0, partLength).join(''));
+    }
+    return parts.join(' ');
+}
