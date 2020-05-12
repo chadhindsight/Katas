f780b4d51e255729e96e754197353dee511943a6
Chad Hinds <chadrickj8@gmail.com>
2020-05-12 10:03:03 -0400

    New kata dealing with space on a display similar to what they have on public transport

--- level-6/Ticker.js
+++ level-6/Ticker.js
@@ -1,4 +1,4 @@
-//Dispay text on a screen with a fixed width
+//Dispay text on a screen with a fixed width similar to bus or subaway scroll text
 const ticker = (text, width, tick) => {
     // Get space to repeat on both left and right sides
     let space = ' '.repeat(width);
