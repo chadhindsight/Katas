cc696270f5d263c4b9a8c14d4fbb4ae4f515bd15
Chad Hinds <chadrickj8@gmail.com>
2020-05-12 09:35:53 -0400

    Get space to repeat on both left and right sides

--- level-6/Ticker.js
+++ level-6/Ticker.js
@@ -1,6 +1,6 @@
 //Dispay text on a screen with a fixed width
-
 const ticker = (text, width, tick) => {
+    // Get space to repeat on both left and right sides
     let space = ' '.repeat(width);
     text = space + text + space;
     tick = tick % (text.length - width);
