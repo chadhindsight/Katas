66e2c7cad3d70ef0d0ad5361a7c6982a2f1617c5
Chad Hinds <chadrickj8@gmail.com>
2020-05-18 09:15:18 -0400

    Split and then add both sides of an array together.

---
+++ level-6/Split&Add.js
@@ -0,0 +1,12 @@
+
+// Split and then add both sides of an array together.
+function splitAndAdd(arr, n) {
+  if (n <= 0)
+    return arr;
+
+  const ln = arr.length;
+  if ((ln % 2) === 1)
+    arr.unshift(0);
+
+  return splitAndAdd(arr.splice(0, Math.ceil(ln / 2)).map((a, i) => a + arr[i]), n-1);
+}
