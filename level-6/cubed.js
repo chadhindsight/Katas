b5a51cb6f1fb4cdb8775aa81991b566452f79580
Chad Hinds <chadrickj8@gmail.com>
2020-05-22 09:27:49 -0400

    new Kata using power of 3

--- level-6/cubed.js
+++ level-6/cubed.js
@@ -1,2 +1,11 @@
 // You are given the total volume m of the building. 
 // Being given m can you find the number n of cubes you will have to build?
+function findNb(m) {
+    let n = 0;
+    let sum = 0;
+    while (sum < m) {
+        n++;
+        sum += Math.pow(n, 3);
+    }
+    return sum === m ? n : -1;
+}
