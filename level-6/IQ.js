afe691b7255502c805c8b3853785ebfddfc5b5a6
Chad Hinds <chadrickj8@gmail.com>
2020-05-24 08:58:00 -0400

    New level 6 kata

---
+++ level-6/IQ.js
@@ -0,0 +1,17 @@
+// Which number in the list is only odd or the only even number
+
+function iqTest(numbers) {
+    let str = numbers.split(' ').map(x => parseInt(x))
+
+    // run two filters to check if even or odd
+    let oddArray = []
+    let evenArray = []
+
+    let even = str.filter(x => x % 2 === 0)
+    evenArray.push(even)
+    let odd = str.filter(x => x % 2 !== 0)
+    oddArray.push(odd)
+
+    return odd.length > even.length ? str.indexOf(even[0]) + 1 : str.indexOf(odd[0]) + 1
+    console.log(str.indexOf(odd[0]) + 1)
+}
