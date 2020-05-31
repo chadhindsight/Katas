268b286412ef9a7ef9343fd527e4024585f152af
Chad Hinds <chadrickj8@gmail.com>
2020-05-31 11:14:37 -0400

    New level 6 kata

---
+++ level-6/alphabet.js
@@ -0,0 +1,9 @@
+// Find longest substring in alphabetical order
+function longest(str, max = '') {
+    const current = [...str].reduce((acc, char) => (
+        char >= acc.slice(-1) ?
+            acc + char : (max = acc.length > max.length ?
+                acc : max, char)
+    ), '');
+    return current.length > max.length ? current : max;
+}
