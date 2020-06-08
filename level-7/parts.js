f5eaa2a8776588b8318de12ba3453112753e20ed
Chad Hinds <chadrickj8@gmail.com>
2020-06-08 10:00:56 -0400

    New kata that chesk unique values in an array

--- level-7/parts.js
+++ level-7/parts.js
@@ -1,7 +1,7 @@
 // The aim of this kata is to split a given string into different strings of equal size (note size of strings is passed to the method)
 var splitInParts = function (s, partLength) {
-    var parts = [];
-    var array = s.split('');
+    let parts = [];
+    let array = s.split('');
     while (array.length) {
         parts.push(array.splice(0, partLength).join(''));
     }
