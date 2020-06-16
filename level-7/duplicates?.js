678d1841aa7b6661444ad37c7636c770c631441b
Chad Hinds <chadrickj8@gmail.com>
2020-06-16 12:39:06 -0400

    Garbage

---
+++ level-7/duplicates?.js
@@ -0,0 +1,11 @@
+// Check if an array has duplicates using frequency counter method
+function areThereDuplicates() {
+    let collection = {}
+    for (let val in arguments) {
+        collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+    }
+    for (let key in collection) {
+        if (collection[key] > 1) return true
+    }
+    return false;
+}
