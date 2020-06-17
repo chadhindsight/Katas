c5cb06534126cba66b55449bb0ab0b8fd7b5881b
Chad Hinds <chadrickj8@gmail.com>
2020-06-17 09:35:26 -0400

    Get the rest of the string

---
+++ level-7/greetMe.js
@@ -0,0 +1,8 @@
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point
+var greet = function (name) {
+    let t = name.toLowerCase().slice(0, 1)
+    // Get the rest of the string
+    let restOfString = name.toLowerCase().slice(1)
+    console.log(rest)
+    return `Hello ${t.toUpperCase()}${restOfString}!`
+};
