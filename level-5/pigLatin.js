59d4d16b715eff81a5321089d780e3f90a60793a
Chad Hinds <chadrickj8@gmail.com>
2020-06-18 09:39:40 -0400

    Account for non letters by using regex

--- level-5/pigLatin.js
+++ level-5/pigLatin.js
@@ -1,11 +1,9 @@
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word
 function pigIt(str) {
-    let arr = str.split(' ')
-
-    //Loop through each word and put first letter & 'ay' towards end 
-    return arr.map(word => {
+    // Account for non letters by using regex
+    return str.replace(/\w+/g, word => {
         let w = word.slice(0, 1)
         let restOfString = word.slice(1)
         return `${restOfString}${w}ay`
-    }).join(' ')
+    })
 }
