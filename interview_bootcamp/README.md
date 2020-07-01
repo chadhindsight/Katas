9287cefe5dabef56a32d2c0019762b34c0b262f1
Chad Hinds <chadrickj8@gmail.com>
2020-07-01 13:56:44 -0400

    interviews

---
+++ interview_bootcamp/README.md
@@ -0,0 +1,3 @@
+<!-- Notes -->
+### NB: Avoid using for loops as much as possible, Use for of, while or .map instead
+### Reduce takes two values: callback and initial starting value
---
+++ interview_bootcamp/exercises/reversestring/index.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+// function reverse(str) {
+//     return str.split('').reverse().join('')
+// }
+
+// SOLUTION 2. Without using reverse method
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let char of str) {
+//         reversed = char + reversed
+//     }
+//     return reversed
+// }
+
+// SOLUTION 3. With .reduce()
+ function reverse(str) {
+    return str.split('').reduce((rev, character)=> character  + rev,'');
+ }
+
+module.exports = reverse;
