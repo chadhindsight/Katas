37af17f31f6218ae241dd7f734e2f6fbfe81b78f
Chad Hinds <chadrickj8@gmail.com>
2020-07-08 14:27:23 -0400

    Solve with recursion

--- interview_bootcamp/exercises/fib/index.js
+++ interview_bootcamp/exercises/fib/index.js
@@ -8,6 +8,26 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+//Iterative. O(n)
+// function fib(n) {
+//     // Create an array with all numbers up to n
+//     let result =[0,1]
+
+//     for(let i =2; i <= n; i++) {
+//         // get previous numbers
+//         const prev = result[i - 1]
+//         const prev2 = result[i - 2]
+//         result.push(prev + prev2)
+//     }
+//     // Return the nth value from the array 
+//     return result[n]
+// }
+
+// Recursion
+function fib(n) {
+    if(n < 2) return n
+
+    return fib(n -1) + fib(n -2)
+}
 
 module.exports = fib;
