8409e5ea6a44b0cd28b7d89f31300968a1f01a24
Chad Hinds <chadrickj8@gmail.com>
2020-07-16 09:49:24 -0400

    Using Switch Statement

--- level-6/likes.js
+++ level-6/likes.js
@@ -1,4 +1,4 @@
-// Return display text that get's formatted based on how many likes an item has
+// Return display a text that get's formatted based on how many likes an item has when given an array of names
 function likes(names) {
     if (names.length === 2) {
         return `${names[0]} and ${names[1]} like this`
@@ -18,3 +18,15 @@ function likes(names) {
     else { return 'no one likes this' }
 
 }
+
+// Using Switch Statement
+// function likes(names) {
+//     names = names || [];
+//     switch (names.length) {
+//         case 0: return 'no one likes this'; break;
+//         case 1: return names[0] + ' likes this'; break;
+//         case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+//         case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+//         default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
+//     }
+// }
