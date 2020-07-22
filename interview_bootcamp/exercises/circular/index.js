6996313c45255403704e9bb92ae42d935c2a5dd7
Chad Hinds <chadrickj8@gmail.com>
2020-07-22 11:00:07 -0400

    complete fromlast exercise

--- interview_bootcamp/exercises/circular/index.js
+++ interview_bootcamp/exercises/circular/index.js
@@ -13,7 +13,16 @@
 //   circular(l) // true
 
 function circular(list) {
-    
+    let slow = list.getFirst()
+    let fast = list.getFirst()
+
+    while(fast.next && fast.next.next) {
+        slow = slow.next
+        fast = fast.next.next
+
+        if(slow === fast) {return true}
+    }
+    return false
 }
 
 module.exports = circular;
--- interview_bootcamp/exercises/fromlast/index.js
+++ interview_bootcamp/exercises/fromlast/index.js
@@ -11,6 +11,21 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    // Both variables start at the first node, then move fast up by a certain number of spaces
+    let slow = list.getFirst()
+    let fast = list.getFirst()
+    
+    while(n > 0) {
+        fast = fast.next;
+        n--
+    }
+    //Next step: move both variables up by 1 until fast hits the end node and then check diff between fast and slow
+    while (fast.next) {
+        slow = slow.next
+        fast = fast.next
+    }
+    return slow
+}
 
 module.exports = fromLast;
