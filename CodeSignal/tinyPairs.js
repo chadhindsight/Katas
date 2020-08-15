b37cb63c27820d3ea8b53657c27b2f6cff9d56cb
Chad Hinds <chadrickj8@gmail.com>
2020-08-15 14:07:42 -0400

    Fix bug that appears when checking second arr

--- CodeSignal/tinyPairs.js
+++ CodeSignal/tinyPairs.js
@@ -1,19 +1,27 @@
-
-// Given two arrays, find the tiny pairs less than a given var(third argument)
+// Given two arrays, find the amount of tiny pairs (x,y)
+// where x is from first array and y is from the second
 let arr1 = [16, 1, 4, 2, 14]
 let arr2 = [7, 11, 2, 0, 16]
 
-let tinyPairs = 0
-function countTiny(first, second, val) {
-    // a = left to right, b = right to left
-    let k = second.length
-    for (let i = 0; i < first.length; i++ && k--) {
-        let pair = parseInt("" + first[i] + second[k])
+
+
+function countTiny(firstArr, secondArr, val) {
+    // mutiple pointers
+    let keyB = secondArr.length;
+    let tinyPairs = 0
+
+    //Edge cases
+    if (firstArr.length === 0 || firstArr.length !== secondArr.length) console.log(0)
+
+    // Everything else
+    for (i = 0; i < firstArr.length; i++) {
+        keyB--
+        let pair = parseInt("" + firstArr[i] + secondArr[keyB])
         if (pair < val) { tinyPairs++ }
     }
     console.log(tinyPairs)
 }
 
 
-countTiny([1, 2, 3], [1, 2, 3], 31)
-// Output should be the number of pairs
+countTiny(arr1, arr2, 743)
+// Output should be number of pairs
