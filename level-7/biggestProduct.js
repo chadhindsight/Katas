0c235c4fff2ff4aee96789185ac9400c31a351eb
Chad Hinds <chadrickj8@gmail.com>
2020-08-19 11:22:44 -0400

    Remove code signal

---
+++ level-7/biggestProduct.js
@@ -0,0 +1,16 @@
+// Given an array of integers, find the pair of adjacent 
+// elements that has the largest product and return that product.
+function adjacentElementsProduct(inputArray) {
+    var arr = inputArray;
+    var x = 0;
+    var y = 0;
+    var p = Number.MIN_SAFE_INTEGER;
+    for (var i = 0; i < arr.length; i++) {
+        x = arr[i];
+        y = arr[i + 1];
+        if (x * y > p) {
+            p = x * y;
+        };
+    };
+    return p;
+}
