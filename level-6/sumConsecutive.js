e0fb35bdd9a12735a5ac27f33f61fa4a81331c7f
Chad Hinds <chadrickj8@gmail.com>
2020-08-25 10:02:13 -0400

    Sum only the numbers that are the same

---
+++ level-6/sumConsecutive.js
@@ -0,0 +1,14 @@
+// Sum only the numbers that are the same and consecutive. The result should be one array.
+
+function sumConsecutives(s) {
+    var sum = 0,
+        sums = [];
+    for (var i = 0; i < s.length; i++) {
+        sum += s[i];
+        if (s[i] != s[i + 1]) {
+            sums.push(sum);
+            sum = 0;
+        }
+    }
+    return sums;
+}
