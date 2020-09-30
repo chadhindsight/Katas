758e3640f1e2d8e816380ce9e79c04b0facf6132
Chad Hinds <chadrickj8@gmail.com>
2020-09-30 10:55:38 -0400

    Remove _

--- level-7/reverseOnlyLetters.js
+++ level-7/reverseOnlyLetters.js
@@ -1,5 +1,5 @@
 // Given a string str, reverse it omitting all non - alphabetic characters.
 function reverseLetter(str) {
-    let changed = str.replace(/[^a-zA-Z_]/gi, '')
+    let changed = str.replace(/[^a-zA-Z]/gi, '')
     return changed.split('').reverse().join('')
 }
