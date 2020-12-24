f2e43ce1044e27704e413d265c4ee82340f01629
Chad Hinds <chadrickj8@gmail.com>
2020-12-24 12:16:02 -0500

    Make comments for new kata more verbose

--- level-6/insertInTheMiddle.js
+++ level-6/insertInTheMiddle.js
@@ -1,6 +1,6 @@
 function tuckIn(arr1, arr2) {
     //Create a function that takes two arrays and 
-    //insert the second array in the middle of the first array
+    //insert the second array in the MIDDLE of the first array
     arr1.splice(Math.round(arr1.length / 2), 0, ...arr2)
 
     return arr1
