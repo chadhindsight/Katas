a5878503087e5eb433f90338baaa35f0cd363b0c
Chad Hinds <chadrickj8@gmail.com>
2020-12-24 12:15:25 -0500

    Add a new Kata that deals with index postions

---
+++ level-6/insertInTheMiddle.js
@@ -0,0 +1,7 @@
+function tuckIn(arr1, arr2) {
+    //Create a function that takes two arrays and 
+    //insert the second array in the middle of the first array
+    arr1.splice(Math.round(arr1.length / 2), 0, ...arr2)
+
+    return arr1
+}
